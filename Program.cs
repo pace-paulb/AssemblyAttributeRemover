dc332db8cb69cf8a795e061ecd46e925de623e41
Paul Buerschaper <48753478+pace-paulb@users.noreply.github.com>
2025-09-06 23:49:19 +0200

    initial commit (version 0.1.5)

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+using Mono.Cecil;
+
+if (args.Length < 2)
+{
+	Console.WriteLine(@"Usage: 
+RemoveAssemblyAttributes <input.dll> <comma-separated list of attributes [full names] to remove>
+
+Example:
+RemoveAssemblyAttributes System.Data.SQLite.EF6.dll System.Security.AllowPartiallyTrustedCallersAttribute
+");
+	return 0;
+}
+
+if (string.IsNullOrWhiteSpace(args[0]))
+	throw new ArgumentException("You did not specify a DLL to be patched.");
+
+var inputDll = new FileInfo(args[0]);
+
+var attributesToRemove = args[1].Split(',');
+
+if (!inputDll.Exists)
+	throw new ArgumentException($"Could not find DLL: {inputDll.FullName}");
+
+var outputDll = new FileInfo($"{inputDll.FullName}.{Guid.NewGuid()}");
+
+if (attributesToRemove.Length == 0)
+	throw new ArgumentException("No attributes specified that should be removed");
+
+var anyAttributeRemoved = false;
+
+using (var assembly = AssemblyDefinition.ReadAssembly(
+			inputDll.FullName,
+			new ReaderParameters {ReadWrite = false}
+		))
+{
+	var assemblyCustomAttributes = assembly.CustomAttributes;
+	var matchedAttributes = assemblyCustomAttributes
+		.Where(a => attributesToRemove.Contains(a.AttributeType.FullName))
+		.ToArray();
+
+	foreach (var matchedAttribute in matchedAttributes)
+	{
+		Console.WriteLine($"Removing [{matchedAttribute.AttributeType.FullName}] from {inputDll}");
+		assemblyCustomAttributes.Remove(matchedAttribute);
+		anyAttributeRemoved = true;
+    }
+
+	if (!anyAttributeRemoved)
+	{
+		Console.WriteLine("No changes. No attribute found that should be removed. Done.");
+		return 0;
+	}
+
+	// 1. write to temp DLL
+    assembly.Write(outputDll.FullName);
+}
+
+// 2. replace original DLL
+outputDll.Replace(inputDll.FullName, null);
+
+Console.WriteLine($"Patched assembly written back to {inputDll.FullName}");
+return 0;
---
+++ docs/README.md
@@ -0,0 +1,33 @@
+﻿# AssemblyAttributeRemover
+
+This nuget package automatically removes unwanted attributes from assemblies in your bin folder upon build.
+
+## Motivation
+For example, another nuget package might install a DLL with unwanted attributes. 
+Instead of all the tedious steps of patching the DLL manually, creating a fork of the old nuget package, and maintaining it forever,
+just use this nuget package.
+
+# Configuration
+By itself, adding the nuget package does nothing. 
+You configure its actions in your `.csproj` file, specifying which attributes should be removed from which assemblies.
+By using normal MsBuild conditions, you can fine-grain the scenario as much as you like.
+
+# Example
+
+```  
+<Project Sdk="Microsoft.NET.Sdk">
+...
+  <ItemGroup>
+    <PackageReference Include="AssemblyAttributeRemover" Version="1.0.0" />
+    <AssemblyAttributeRemover
+      Condition=" '$(TargetFramework)' == 'net472' "
+      Include="System.Data.SQLite.EF6,System.Security.AllowPartiallyTrustedCallersAttribute" />
+  </ItemGroup>
+...
+</Project>
+```
+
+Adding these lines to a `.csproj` file will have the following effect:
+
+Upon build of .NET Framework 4.7.2, will look inside the `bin` folder for a DLL called `System.Data.SQLite.EF6.dll`, 
+and - if found - will remove all attributes of type `AllowPartiallyTrustedCallersAttribute` from it, resaving the DLL in-place.
